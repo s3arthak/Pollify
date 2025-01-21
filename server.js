ab4c9d09e80ce1194a2866cbf6e90815e6577d8c
Sarthak Kharka <kharkasarthak@gmail.com>
2025-01-21 19:50:52 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/User'); 
-const pollRoutes = require('./routes/Poll'); 
+const authRoutes = require('./backend/routes/User'); 
+const pollRoutes = require('./backend/routes/Poll'); 
+const dotenv=require('dotenv');
+
+dotenv.config()
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+// 'mongodb://localhost:27017/votingapp'
 
-
-mongoose.connect('mongodb://localhost:27017/votingapp', {
+mongoose.connect(process.env.MONGOURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
