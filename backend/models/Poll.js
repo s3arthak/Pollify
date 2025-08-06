0a8cfe70de9bf51c0daaf6f3b798315333b44299
Sarthak Kharka <143311247+s3arthak@users.noreply.github.com>
2025-08-06 17:13:51 +0530

    added expiry date in model

--- backend/models/Poll.js
+++ backend/models/Poll.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const optionSchema = new mongoose.Schema({
     text: String,
-    votes: { type: Number, default: 0 } // Corrected the votes field
+    votes: { type: Number, default: 0 }
 });
 
 const pollSchema = new mongoose.Schema({
-    question: { type: String, required: true }, // Fixed 'requires' to 'required'
+    question: { type: String, required: true },
     options: [optionSchema],
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    expiresAt: { type: Date, required: false },
 });
-
 
 module.exports = mongoose.model('Poll', pollSchema);
